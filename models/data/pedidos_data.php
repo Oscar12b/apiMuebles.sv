b2b639151ccf893ccae9f6cf33f8bbff0ac06173
smonterroza <20190288@ricaldone.edu.sv>
2024-05-14 18:21:55 -0600

    Ya estan creadas las carpetas

---
+++ models/data/pedidos_data.php
@@ -0,0 +1,5 @@
+<?php
+// Se incluye la clase para validar los datos de entrada.
+require_once('../../helpers/validator.php');
+// Se incluye la clase padre.
+require_once('../../models/handler/pedidos_handler.php');
