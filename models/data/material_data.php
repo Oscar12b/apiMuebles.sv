b5b9bf32408a7d31803111b82885f8fd4e28c224
Rebeca Marcela <20220240@ricaldone.edu.sv>
2024-05-15 23:31:07 -0600

    php de materiales

---
+++ models/data/material_data.php
@@ -0,0 +1,68 @@
+<?php
+// Se incluye la clase para validar los datos de entrada.
+require_once('../../helpers/validator.php');
+// Se incluye la clase padre.
+require_once('../../models/handler/material_handler.php');
+/*
+ *  Clase para manejar el encapsulamiento de los datos de la tabla CATEGORIA.
+ */
+class MaterialData extends MaterialHandler
+{
+    /*
+     *  Atributos adicionales.
+     */
+    private $data_error = null;
+    private $filename = null;
+
+    /*
+     *  Métodos para validar y establecer los datos.
+     */
+    public function setId($value)
+    {
+        if (Validator::validateNaturalNumber($value)) {
+            $this->id = $value;
+            return true;
+        } else {
+            $this->data_error = 'El identificador de la categoría es incorrecto';
+            return false;
+        }
+    }
+
+    public function setNombre($value, $min = 2, $max = 50)
+    {
+        if (!Validator::validateAlphanumeric($value)) {
+            $this->data_error = 'El nombre debe ser un valor alfanumérico';
+            return false;
+        } elseif (Validator::validateLength($value, $min, $max)) {
+            $this->nombre = $value;
+            return true;
+        } else {
+            $this->data_error = 'El nombre debe tener una longitud entre ' . $min . ' y ' . $max;
+            return false;
+        }
+    }
+
+    public function setFilename()
+    {
+        if ($data = $this->readFilename()) {
+            $this->filename = $data['imagen_categoria'];
+            return true;
+        } else {
+            $this->data_error = 'Categoría inexistente';
+            return false;
+        }
+    }
+
+    /*
+     *  Métodos para obtener el valor de los atributos adicionales.
+     */
+    public function getDataError()
+    {
+        return $this->data_error;
+    }
+
+    public function getFilename()
+    {
+        return $this->filename;
+    }
+}
