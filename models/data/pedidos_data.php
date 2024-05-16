dcba36c05b0fc28970e68e511d6050ea5e7da64a
smonterroza <20190288@ricaldone.edu.sv>
2024-05-16 07:54:25 -0600

    Agregue lo de pedidos_data

--- models/data/pedidos_data.php
+++ models/data/pedidos_data.php
@@ -2,4 +2,26 @@
 // Se incluye la clase para validar los datos de entrada.
 require_once('../../helpers/validator.php');
 // Se incluye la clase padre.
-require_once('../../models/handler/pedidos_handler.php');
+require_once('../../models/handler/pedido_handler.php');
+
+class PedidoData extends PedidoHandler
+{
+    private $data_error = null;
+
+    public function setId($value)
+    {
+        if (Validator::validateNaturalNumber($value)) {
+            $this->id = $value;
+            return true;
+        } else {
+            $this->data_error = 'El id del pedido es incorrecto';
+            return false;
+        }
+    }
+
+    public function getDataError()
+    {
+        return $this->data_error;
+    }
+}
+?>
--- models/handler/pedidos_handler.php
+++ models/handler/pedidos_handler.php
@@ -1,34 +1,73 @@
 <?php
 // Se incluye la clase para trabajar con la base de datos.
-require_once('../../helpers/database.php');
+require_once ('../../helpers/database.php');
+
 /*
  *  Clase para manejar el comportamiento de los datos de la tabla pedidos.
  */
-class PedidosHandler
+class PedidoHandler
 {
     /*
      *  Declaración de atributos para el manejo de datos.
      */
-    protected $id_pedido = null;
-    protected $estado_pedido = null;
-    protected $fecha_pedido = null;
-    protected $fecha_entrega = null;
-    protected $direccion_pedido = null;
-    protected $id_cliente = null;
+    protected $id = null;
+    protected $estado = null;
+    protected $fechaEntrega = null;
+    protected $direccion = null;
 
     /*
-    *   Métodos para realizar las operaciones SCRUD (search, create, read, update, and delete).
-    */
-
-    public function searchRows()
+     *  Métodos para cambiar el estado de un pedido.
+     */
+    public function cambiarEstado()
     {
-        $value = '%' . Validator::getSearchValue() . '%';
-        $sql = 'SELECT id_pedido, estado_pedido, fecha_pedido, fecha_entrega, direccion_pedido, nombre_cliente
-                FROM tb_pedidos
-                INNER JOIN cliente USING(id_cliente)
-                WHERE estado_pedido LIKE ? OR fecha_pedido LIKE ? OR fecha_entrega LIKE ? OR direccion_pedido LIKE ?
-                ORDER BY fecha_pedido';
-        $params = array($value, $value, $value, $value);
+        $sql = 'UPDATE tb_pedidos
+                SET estado_pedido = ?
+                WHERE id_pedido = ?';
+        $params = array($this->estado, $this->id);
+        return Database::executeRow($sql, $params);
+    }
+
+    /*
+     *  Métodos para eliminar un pedido.
+     */
+    public function eliminarPedido()
+    {
+        $sql = 'DELETE FROM tb_pedidos
+                WHERE id_pedido = ?';
+        $params = array($this->id);
+        return Database::executeRow($sql, $params);
+    }
+
+    /*
+     *  Métodos para buscar detalles de un pedido.
+     */
+    public function buscarDetallesPedido()
+    {
+        $sql = 'SELECT tb_detalles_pedidos.id_detalle_pedido, tb_detalles_pedidos.cantidad_pedido, tb_detalles_pedidos.precio_pedido, tb_muebles.nombre_mueble
+                FROM tb_detalles_pedidos
+                INNER JOIN tb_muebles ON tb_detalles_pedidos.id_mueble = tb_muebles.id_mueble
+                WHERE tb_detalles_pedidos.id_pedido = ?';
+        $params = array($this->id);
         return Database::getRows($sql, $params);
     }
+
+    /*
+     *  Métodos para realizar las operaciones de búsqueda y cambio de estado.
+     */
+    public function buscarPedidos()
+    {
+        $sql = 'SELECT id_pedido, estado_pedido, fecha_pedido, fecha_entrega, direccion_pedido
+                FROM tb_pedidos';
+        return Database::getRows($sql);
+    }
+
+    public function cambiarEstadoPedido()
+    {
+        $sql = 'UPDATE tb_pedidos
+                SET estado_pedido = ?
+                WHERE id_pedido = ?';
+        $params = array($this->estado, $this->id);
+        return Database::executeRow($sql, $params);
+    }
 }
+?>
